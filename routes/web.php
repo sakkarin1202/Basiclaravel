6e1876de4a9e04752b5002c61a13f300227e3861
sakkarin1202 <xfake12340@hotmail.com>
2024-07-26 10:18:38 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController; 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+Route::get('/posts/message', 'App\Http\Controllers\PostController@message')->name('posts.message');
+Route::get('/posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
+Route::post('/posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
